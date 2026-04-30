71e42e1c531cfd6d7eae0a33966a97d13610c1ea
hatedabamboo <github.yayza@passmail.com>
2026-04-30 17:16:05 +0200

    (chore) Add changelog and its generation

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 2026-04-24
+
+- (release) bumpwf goes live! [713b158](https://github.com/hatedabamboo/bumpwf/commit/713b158d9fdc6f91da283b9da7de488a263615e1)
---
+++ changelog.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+REMOTE=$(git remote get-url origin 2>/dev/null | sed 's/git@github.com:/https:\/\/github.com\//; s/\.git$//')
+
+if [[ -z "$REMOTE" ]]; then
+    echo "Error: could not determine remote URL" >&2
+    exit 1
+fi
+
+git log --pretty=format:"%H %ad %s" --date=short | grep -viE "\.(md)$|FUNDING" | awk -v repo="$REMOTE" '
+BEGIN { prev_date = "" }
+{
+    sha=$1; date=$2; short=substr(sha,1,7)
+    msg=""; for(i=3;i<=NF;i++) msg=(msg==""?$i:msg" "$i)
+    type="chore"
+    if (match(msg,/^(feat|fix|refactor|docs|test|chore|style|perf|ci|build)[(:!]/,arr)) {
+        type=arr[1]; sub(/^[a-z]+(\([^)]*\))?!?:\s*/,"",msg)
+    }
+    if (date!=prev_date) { if(prev_date!="") print ""; print "## "date; prev_date=date }
+    printf "- (%s) %s [%s](%s/commit/%s)\n", type, msg, short, repo, sha
+}
+'
